3718c607cd17b05a70191bd9c5260411d2b8b570
Carlos Damian Chavez Gonalez <carlos_dami12@live.com>
2021-11-18 17:13:36 -0600

    login terminado

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,45 +1,76 @@
 const usersCtrl = {};
 const userModel = require('../models/users');
+const bcrypt = require('bcrypt');
+const {genSalt} = require("bcrypt");
 
 usersCtrl.getUsers = async (req, res) => {
     try{
         const users = await userModel.find();
-        res.json({users, status:200});
+        res.status(200).json(users);
     }catch (e) {
-        res.json({status: 400});
+        res.status(400);
     }
 };
 
 usersCtrl.getUser = async (req, res) => {
     try{
         const user = await userModel.findById(req.params.id)
-        res.json({user, status: 200});
+        res.status(200).json(user);
     }catch (e) {
-        res.json({status: 400});
+        res.status(400);
     }
 };
 
 usersCtrl.createUser = async (req, res) => {
     try{
         const {name, phone, img, age, email, priority, problem, promotion, curp, password} = req.body
-        const newUser = new userModel({
-            name,
-            phone,
-            img,
-            age,
-            email,
-            priority,
-            problem,
-            promotion,
-            curp,
-            password,
-        });
 
-        await newUser.save();
-        res.json({newUser, status: 200});
+        await bcrypt.hash(password, 10, async (err, hashedPassword) => {
+            if (err) {
+                res.status(400);
+            } else {
+                const newUser = new userModel({
+                    name,
+                    phone,
+                    img,
+                    age,
+                    email,
+                    priority,
+                    problem,
+                    promotion,
+                    curp,
+                    password: hashedPassword,
+                });
+
+                console.log(newUser)
+
+                await newUser.save();
+                res.status(200).json(newUser);
+            }
+        })
     }catch (e) {
-        res.json({status: 400});
+        res.status(400);
     }
 };
+
+usersCtrl.loginUser = async (req, res) => {
+    try{
+        userModel.findOne({email: req.body.email}).then((e) => {
+            bcrypt.compare(req.body.password, e.password, (err, same) => {
+                console.log(err)
+                console.log(same)
+                if (err){
+                    res.status(500).send('Error al Autenticar');
+                }else if (same){
+                    res.status(200).json(e);
+                }else{
+                    res.status(400).send('Usuario o contraseña incorrecto');
+                }
+            });
+        })
+    }catch (e) {
+        res.status(400);
+    }
+}
 
 module.exports = usersCtrl;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,13 @@
 const {Router} = require('express');
 const router = Router();
-const {getUsers, createUser, getUser} = require('../controllers/users.controller');
+const {getUsers, createUser, getUser, loginUser} = require('../controllers/users.controller');
 
 router.route('/')
     .get(getUsers)
     .post(createUser)
+
+router.route('/login')
+    .post(loginUser)
 
 router.route('/:id')
     .get(getUser)
